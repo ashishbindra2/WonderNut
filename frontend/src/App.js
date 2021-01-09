3d1a0f326722567d4c43b9eac072c1e4217ec0cd
unknown <ashishbindra2>
2021-01-09 21:57:06 +0530

    first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
+import { Container } from 'react-bootstrap'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 // function App() = const App = () =>{}
 function App() {
   return (
-    <>
-    <h1>welcome to wonder</h1>
-    </>
+    
+    <Router>
+    <Header/>
+    <main className="py-1">
+      <Container>  
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+
+       </Container>
+       </main>
+ <Footer/>
+    </Router>
   );
 }
 
